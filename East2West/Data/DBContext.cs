54fde31220909fdeffd698df82b9ec4126226e4e
nmq126 <minhquangngo.98@gmail.com>
2022-06-17 17:10:02 +0700

    db context

---
+++ East2West/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace East2West.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+            : base("name=DBContext")
+        {
+        }
+    }
+}
