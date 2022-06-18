104afb91d4fc1fde11b2f45e0d31b73a7b316f7c
nmq126 <minhquangngo.98@gmail.com>
2022-06-18 13:50:00 +0700

    update bo sung

---
+++ East2West/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace East2West.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<East2West.Data.DBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(East2West.Data.DBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
