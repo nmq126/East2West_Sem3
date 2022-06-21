71ed39e01ff23797e0aecbd50c8c114d12dba1aa
nmq126 <minhquangngo.98@gmail.com>
2022-06-21 19:21:57 +0700

    update edit detail tour

--- East2West/Controllers/ToursController.cs
+++ East2West/Controllers/ToursController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -17,15 +18,17 @@ namespace East2West.Controllers
         private DBContext db = new DBContext();
 
         // GET: Tours
-        public ActionResult Index(string id, string sortType, string keyword, string departureId, string destinationId, int? page)
+        public ActionResult Index(int? status, string id, string sortType, string keyword, string duration_range, string departureId, string destinationId, int? page)
         {
             ViewBag.BreadCrumb = "Tour List";
             var tours = from t in db.Tours select t;
 
             ViewBag.LocationList = from l in db.Locations select l;
+            ViewBag.Status = status;
             ViewBag.Keyword = keyword;
             ViewBag.Id = id;
             ViewBag.SortType = sortType;
+            ViewBag.DurationRange = duration_range;
             ViewBag.DepartureId = departureId;
             ViewBag.DestinationId = destinationId;
             int pageNumber = (page ?? 1);
@@ -51,6 +54,35 @@ namespace East2West.Controllers
                 tours = tours.Where(t => t.DestinationId == destinationId);
             }
 
+            switch (status)
+            {
+                case 2:
+                    break;
+                case 1:
+                    tours = tours.Where(t => t.Status == 1);
+                    break;
+                case 0:
+                    tours = tours.Where(t => t.Status == 0);
+                    break;
+                default:
+                    break;
+            }
+            switch (duration_range)
+            {
+                case "all":
+                    break;
+                case "lte3":
+                    tours = tours.Where(t => t.Duration <= 3);
+                    break;
+                case "4to7":
+                    tours = tours.Where(t => t.Duration >= 4 && t.Duration <= 7);
+                    break;
+                case "gt7":
+                    tours = tours.Where(t => t.Duration > 7);
+                    break;
+                default:
+                    break;
+            }
             switch (sortType)
             {
                 case "createdAt_asc":
@@ -81,11 +113,13 @@ namespace East2West.Controllers
         // GET: Tours/Details/5
         public ActionResult Details(string id)
         {
+            ViewBag.BreadCrumb = "Tour detail";
+
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Tour tour = db.Tours.Find(id);
+            Tour tour = db.Tours.Include(t => t.TourSchedules).FirstOrDefault(t => t.Id == id);
             if (tour == null)
             {
                 return HttpNotFound();
@@ -158,11 +192,13 @@ namespace East2West.Controllers
         // GET: Tours/Edit/5
         public ActionResult Edit(string id)
         {
+            ViewBag.BreadCrumb = "Edit Tour";
+
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Tour tour = db.Tours.Find(id);
+            Tour tour = db.Tours.Include(t => t.TourSchedules).FirstOrDefault(t => t.Id == id);
             if (tour == null)
             {
                 return HttpNotFound();
@@ -182,6 +218,11 @@ namespace East2West.Controllers
             if (ModelState.IsValid)
             {
                 db.Entry(tour).State = EntityState.Modified;
+                tour.UpdatedAt = DateTime.Now;
+                if (tour.Status == 0)
+                {
+                    tour.DeletedAt = DateTime.Now;
+                }
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -190,30 +231,20 @@ namespace East2West.Controllers
             return View(tour);
         }
 
-        // GET: Tours/Delete/5
-        public ActionResult Delete(string id)
+        public String ChangeStatus(string id, int status)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return "Bad Request";
             }
             Tour tour = db.Tours.Find(id);
             if (tour == null)
             {
-                return HttpNotFound();
+                return "Tour not found";
             }
-            return View(tour);
-        }
-
-        // POST: Tours/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            Tour tour = db.Tours.Find(id);
-            db.Tours.Remove(tour);
+            tour.Status = status;
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return "Update success";
         }
 
         protected override void Dispose(bool disposing)
