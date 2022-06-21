b6068e31a9e3e1f1f0dc5d49a269b512a27695c8
anh s <jimmianh1807@gmail.com>
2022-06-21 20:44:11 +0700

    commit

---
+++ East2West/Models/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace East2West.Models
+{
+    public class AppRole : IdentityRole
+    {
+        public string Description { get; set; }
+    }
+}
