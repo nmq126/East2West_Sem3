e024edd51553af24beb48acd536dee1f040e8d61
vungoctan1109 <vntan1109@gmail.com>
2022-06-28 17:08:30 +0700

    add list car

---
+++ East2West/Controllers/ClientCarController.cs
@@ -0,0 +1,27 @@
+﻿using East2West.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
+using East2West.Models;
+using Microsoft.AspNet.Identity;
+
+namespace East2West.Controllers
+{
+    public class ClientCarController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        public ActionResult GetListCar()
+        {
+            var cars = db.Cars.Include(c => c.CarModel)
+                .Include("CarModel.CarBrand")
+                .Include(c => c.CarType)
+                .Include(c => c.Location);
+            return View(cars.ToList());
+        }
+    }
+}
