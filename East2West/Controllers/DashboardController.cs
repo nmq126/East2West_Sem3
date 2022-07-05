4a2317dbac56c14591df8fb9d67c6988ee73dbbe
nmq126 <minhquangngo.98@gmail.com>
2022-07-05 13:19:36 +0700

    add dashboard date range

--- East2West/Controllers/DashboardController.cs
+++ East2West/Controllers/DashboardController.cs
@@ -12,6 +12,10 @@ namespace East2West.Controllers
     public class DashboardController : Controller
     {
         private DBContext db = new DBContext();
+        private readonly string tourDurationTitle = "Revenue by tour package duration share";
+        private readonly string tourCategoryTitle = "Revenue by tour package category share";
+        private readonly string tourSeasonTitle = "Revenue by season share";
+        private static string tourDateRange;
         // GET: Dashboard
         public ActionResult Index()
         {
@@ -19,133 +23,171 @@ namespace East2West.Controllers
             ViewBag.TotalOrder = db.Orders.Count();
             ViewBag.TotalUser = db.Users.Count();
             ViewBag.TotalRevenue = db.Orders.Where(x => x.Status == 1).Sum(x => x.TotalPrice);
-
-            ViewBag.PieChartTitle = "Revenue by tour package duration share";
-
-            ViewBag.DataPoints = GetOrderTourDataByDuration();
+            tourDateRange = "All time";
+            ViewBag.DataPoints = GetOrderTourDataByDuration(db.Orders.Where(x => x.Type == 1 && x.Status == 1));
 
             return View();
         }
 
-        private String GetOrderTourDataByDuration()
+        public String GetTourChartData(string typeId, string startDay, string endDay)
         {
+            var orders = GetOrderInDateRange(startDay, endDay);
+            if (typeId == "2")
+            {
+                return GetOrderTourDataByDuration(orders);
+            }
+            if (typeId == "1")
+            {
+                return GetOrderTourDataByCategory(orders);
+            }
+            if (typeId == "0")
+            {
+                return GetOrderTourDataBySeason(orders);
+            }
+            return null;
+        }
+
+        public IQueryable<Models.Order> GetOrderInDateRange(string startDay, string endDay)
+        {
+            var orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1);
+            if (!String.IsNullOrWhiteSpace(startDay) && !String.IsNullOrWhiteSpace(endDay))
+            {
+                var start = DateTime.Parse(startDay);
+                var end = DateTime.Parse(endDay);
+                orders = orders.Where(o => o.CreatedAt >= start && o.CreatedAt <= end);
+                tourDateRange = startDay + " - " + endDay;
+            }
+            return orders;
+        }
+
+        private String GetOrderTourDataByDuration(IQueryable<Models.Order> orders)
+        {
+            if (!orders.Any())
+            {
+                return null;
+            }
             List<DataPoint> dataPoints = new List<DataPoint>();
-            var orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration <= 3);
-            if (orders == null || orders.Count() == 0)
+            var ordersItem = orders.Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration <= 3);
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("Less than 3 days", 0));
 
             }
             else
             {
-                dataPoints.Add(new DataPoint("Less than 3 days", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("Less than 3 days", ordersItem.Sum(o => o.TotalPrice)));
             }
-            orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration >= 4 && o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration <= 7);
-            if (orders == null || orders.Count() == 0)
+            ordersItem = orders.Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration >= 4 && o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration <= 7);
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("4 to 7 days", 0));
             }
             else
             {
-                dataPoints.Add(new DataPoint("4 to 7 days", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("4 to 7 days", ordersItem.Sum(o => o.TotalPrice)));
             }
-            orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration > 7);
-            if (orders == null || orders.Count() == 0)
+            ordersItem = orders.Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.Duration > 7);
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("Over 7 days", 0));
 
             }else
             {
-                dataPoints.Add(new DataPoint("Over 7 days", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("Over 7 days", ordersItem.Sum(o => o.TotalPrice)));
             }
-
-            return JsonConvert.SerializeObject(dataPoints);
+            var obj = new
+            {
+                title = tourDurationTitle,
+                date = tourDateRange,
+                data = dataPoints
+            };
+            return JsonConvert.SerializeObject(obj);
         }
 
-        private String GetOrderTourDataByCategory()
+        private String GetOrderTourDataByCategory(IQueryable<Models.Order> orders)
         {
+            if (!orders.Any())
+            {
+                return null;
+            }
             List<DataPoint> dataPoints = new List<DataPoint>();
             var categories = from c in db.TourCategories select c;
             foreach (var item in categories)
             {
-                var orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.TourCategoryId == item.Id);
-                if (orders == null || orders.Count() == 0)
+                var ordersItem = orders.Where(o => o.OrderTours.FirstOrDefault().TourDetail.Tour.TourCategoryId == item.Id);
+                if (ordersItem == null || ordersItem.Count() == 0)
                 {
                     dataPoints.Add(new DataPoint(item.Name, 0));
 
                 }
                 else
                 {
-                    dataPoints.Add(new DataPoint(item.Name, orders.Sum(o => o.TotalPrice)));
+                    dataPoints.Add(new DataPoint(item.Name, ordersItem.Sum(o => o.TotalPrice)));
                 }
             }
-            
-            return JsonConvert.SerializeObject(dataPoints);
+            var obj = new
+            {
+                title = tourCategoryTitle,
+                date = tourDateRange,
+                data = dataPoints
+            };
+            return JsonConvert.SerializeObject(obj);
         }
 
-        private String GetOrderTourDataBySeason()
+        private String GetOrderTourDataBySeason(IQueryable<Models.Order> orders)
         {
+            if (!orders.Any())
+            {
+                return null;
+            }
             List<DataPoint> dataPoints = new List<DataPoint>();
 
-            var orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 3, 4, 5 }.Contains(x.CreatedAt.Month));
-            if (orders == null || orders.Count() == 0)
+            var ordersItem = orders.Where(x => new[] { 3, 4, 5 }.Contains(x.CreatedAt.Month));
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("Spring", 0));
 
             }
             else
             {
-                dataPoints.Add(new DataPoint("Spring", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("Spring", ordersItem.Sum(o => o.TotalPrice)));
             }
-            orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 6, 7, 8 }.Contains(x.CreatedAt.Month));
-            if (orders == null || orders.Count() == 0)
+            ordersItem = orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 6, 7, 8 }.Contains(x.CreatedAt.Month));
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("Summer", 0));
             }
             else
             {
-                dataPoints.Add(new DataPoint("Summer", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("Summer", ordersItem.Sum(o => o.TotalPrice)));
             }
-            orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 9, 10, 11 }.Contains(x.CreatedAt.Month));
-            if (orders == null || orders.Count() == 0)
+            ordersItem = orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 9, 10, 11 }.Contains(x.CreatedAt.Month));
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("Fall", 0));
 
             }
             else
             {
-                dataPoints.Add(new DataPoint("Fall", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("Fall", ordersItem.Sum(o => o.TotalPrice)));
             }
-            orders = db.Orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 12, 1, 2 }.Contains(x.CreatedAt.Month));
-            if (orders == null || orders.Count() == 0)
+            ordersItem = orders.Where(x => x.Type == 1 && x.Status == 1).Where(x => new[] { 12, 1, 2 }.Contains(x.CreatedAt.Month));
+            if (ordersItem == null || ordersItem.Count() == 0)
             {
                 dataPoints.Add(new DataPoint("Winter", 0));
 
             }
             else
             {
-                dataPoints.Add(new DataPoint("Winter", orders.Sum(o => o.TotalPrice)));
+                dataPoints.Add(new DataPoint("Winter", ordersItem.Sum(o => o.TotalPrice)));
             }
-
-            return JsonConvert.SerializeObject(dataPoints);
+            var obj = new
+            {
+                title = tourSeasonTitle,
+                date = tourDateRange,
+                data = dataPoints
+            };
+            return JsonConvert.SerializeObject(obj);
         }
-
-        public String ChangeType(string typeId)
-        {
-            if (typeId == "2")
-            {
-                return GetOrderTourDataByDuration();
-            }
-            if (typeId == "1")
-            {
-                return GetOrderTourDataByCategory();
-            }
-            if (typeId == "0")
-            {
-                return GetOrderTourDataBySeason();
-            }
-            return null;
-        }
-
     }
 }
